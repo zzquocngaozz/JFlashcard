8facfdfd977bea15e3cefac5ce433cf75e849a0d
HUUDD <doduchuu6@gmail.com>
2023-09-27 20:32:02 +0700

    add entities

---
+++ src/main/java/com/example/jflashcards/entities/Comment.java
@@ -0,0 +1,43 @@
+package com.example.jflashcards.entities;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Admin
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "comment")
+@Entity
+public class Comment implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId; // Trường primary key
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @JsonFormat(pattern="dd-MM-yyyy")
+    private Date createdAt;
+
+    @ManyToOne // Mối quan hệ nhiều Comment đến một ClassPost
+    @JoinColumn(name = "post_id") // Đặt tên cột foreign key là "class_id"
+    private ClassPost classPost;
+
+    @ManyToOne // Mối quan hệ nhiều Comment đến một User
+    @JoinColumn(name = "user_id") // Đặt tên cột foreign key là "class_id"
+    private User user;
+}
---
+++ src/main/java/com/example/jflashcards/entities/FolderSet.java
@@ -0,0 +1,45 @@
+package com.example.jflashcards.entities;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Admin
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "folderset")
+@Entity
+public class FolderSet implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "folderset_id")
+    private Long folderSetId; // Trường primary key
+
+    @Column(name = "created_at")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @JsonFormat(pattern="dd-MM-yyyy")
+    private Date createdAt;
+
+    @ManyToOne // Mối quan hệ nhiều folderset  đến một folder
+    @JoinColumn(name = "folder_id") // Đặt tên cột foreign key là "user_id"
+    private Folder folder;
+
+    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinTable(name = "floder_flashcardset",
+            joinColumns =@JoinColumn(name = "folderset_id"),
+            inverseJoinColumns =@JoinColumn(name = "flashcardset_id"))
+    private Set<FlashcardSet> flashcardSets = new HashSet<>();
+}
