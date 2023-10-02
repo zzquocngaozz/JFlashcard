f85fb599724194b8b80dde9455293402f0f96a47
HUUDD <doduchuu6@gmail.com>
2023-10-02 21:30:40 +0700

    add role database

--- src/main/java/com/example/jflashcards/JFlashcardsApplication.java
+++ src/main/java/com/example/jflashcards/JFlashcardsApplication.java
@@ -1,6 +1,8 @@
 package com.example.jflashcards;
 
+import com.example.jflashcards.entities.Role;
 import com.example.jflashcards.entities.User;
+import com.example.jflashcards.repository.RoleRepository;
 import com.example.jflashcards.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,28 +17,39 @@ public class JFlashcardsApplication implements CommandLineRunner {
     }
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
     @Override
     public void run(String... arg) throws Exception{
-        User user = new User();
-        user.setUserName("huu");
-        user.setPassword("123456789");
-        user.setEmail("huuuu@gmail.com");
-        user.setFirstName("do");
-        user.setLastName("huu");
-        user.setPhone("0123456789");
-        user.setGender(1);
-        user.setBirth("23-06-2001");
-        userRepository.save(user);
-        User user1 = new User();
-        user1.setUserName("hieu");
-        user1.setPassword("123456789");
-        user1.setEmail("hieuu@gmail.com");
-        user1.setFirstName("hoang");
-        user1.setLastName("hieu");
-        user1.setPhone("0124456789");
-        user1.setGender(1);
-        user1.setBirth("12-6-2001");
-        userRepository.save(user1);
+//        Role role = new Role();
+//        role.setName("ROLE_LEARNER");
+//        roleRepository.save(role);
+//        Role role1 = new Role();
+//        role1.setName("ROLE_TEACHER");
+//        roleRepository.save(role1);
+//        Role role2 = new Role();
+//        role.setName("ROLE_ADMIN");
+//        roleRepository.save(role2);
+//        User user = new User();
+//        user.setUserName("huu");
+//        user.setPassword("123456789");
+//        user.setEmail("huuuu@gmail.com");
+//        user.setFirstName("do");
+//        user.setLastName("huu");
+//        user.setPhone("0123456789");
+//        user.setGender(1);
+//        user.setBirth("23-06-2001");
+//        userRepository.save(user);
+//        User user1 = new User();
+//        user1.setUserName("hieu");
+//        user1.setPassword("123456789");
+//        user1.setEmail("hieuu@gmail.com");
+//        user1.setFirstName("hoang");
+//        user1.setLastName("hieu");
+//        user1.setPhone("0124456789");
+//        user1.setGender(1);
+//        user1.setBirth("12-6-2001");
+//        userRepository.save(user1);
     }
 
 }
